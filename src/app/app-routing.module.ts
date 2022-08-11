8a0641791f019d05ad45b527c6dcbb5804274940
eriker75 <eribertmarquez75@gmail.com>
2022-08-11 17:35:39 -0400

    routing implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraphicsComponent } from './pages/graphics/graphics.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: LoginComponent},
+  {path: 'progress', component: ProgressComponent},
+  {path: 'graphics', component: GraphicsComponent},
+  {path: '', component: DashboardComponent},
+  {path: '**', component: NoPageFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
